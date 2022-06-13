b61a18a1c260b07521281a3a4840d9396c45f66a
Cesar Miquel <miquel@easytech.com.ar>
2022-06-13 03:43:07 -0300

    Add logging to serve.sh.

--- serve.sh
+++ serve.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+LOG=/tmp/jekyll-blog.log
+
+if [ -f "$LOG" ]; then
+    rm -f "$LOG"
+fi
+
 export JEKYLL_VERSION=3.8
 docker run --rm \
   --volume="$PWD:/srv/jekyll:Z" \
   --publish [::1]:4000:4000 \
   jekyll/jekyll:$JEKYLL_VERSION \
-  jekyll serve
+  jekyll serve >> "$LOG" &
